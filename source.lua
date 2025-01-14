1fa8165f9a86d355b2afe6be171a7b015ba7a922
puwgen <165362601+puwgen@users.noreply.github.com>
2025-01-14 21:14:32 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -2,27 +2,26 @@
 -- https://github.com/puwgen/global-blacklist
 
 -- WARNING: HttpService must be enabled in order for this script to work
+
+local Version_UpdateError = true -- Set to false if you want the script to not make an error when it's outdated
 local GlobalBlacklist = {
 	Sources = { }, -- Do not touch
-	Manual = { }, -- Do not touch
-	Whitelist = { }, -- Do not touch
+	Manual = { Users = {}; Groups = {} }, -- Do not touch
+	Whitelist = { Users = {}; Groups = {} }, -- Do not touch
 }
 
 
 local GroupService = game:GetService("GroupService")
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
 
 assert(HttpService.HttpEnabled, 'You must enable HttpService for the script to work')
-print("running")
 -- You are free to customize these functions!
 -- Of course, if you know what you are doing
 
-local Callbacks = { }
 
-local Version_UpdateError = true -- Set to false if you want the script to not make an error when it's outdated
 local Version_Current = "1.0.0" -- This must not be touched
-
 local function ApplyChanges()
 	-- This script automatically collects info from the github repository
 	-- Using this, You won't have to constantly update the code that you have
@@ -52,20 +51,97 @@ local function CheckScriptVersion()
 	end
 end
 
+local function ArrayMerge(t1, t2)
+	local res = t1
+	for _, v in t2 do
+		table.insert(res, v)
+	end
+	return res
+end
+
+local function CoreKick(player : Player, reason)
+	player:Kick(string.format(
+		"\n\nYou are not allowed to join the experience\nReason: %s\n\n",
+		reason
+	))
+end
+
 local function ScanUser(id : number)
 	local r = "innocent"
+	local ingameInstance = Players:GetPlayerByUserId(id)
+	
+	local function TerminateSession()
+		Players:BanAsync({
+			UserIds = { id },
+			Duration = -1,
+			DisplayReason = 'Account involved in Terms of Service violation',
+			PrivateReason = "UserBlacklisted"
+		})
+	end
 	for i, source in GlobalBlacklist.Sources do
 		if r ~= "innocent" then
-			break
+			return r
 		end
-		print(source)
 		local success, returned = pcall(function()
-			local groups = GroupService:GetGroupsAsync(source)
+			local groups = {}
+			for _,v in GroupService:GetGroupsAsync(source) do table.insert(groups, v.Id) end
+			for _,v in GlobalBlacklist.Manual.Groups do table.insert(groups, v) end
 			local followings = {}
+			if table.find(GlobalBlacklist.Manual.Users, id) then
+				task.spawn(TerminateSession)
+				return r
+			end
 			
+			do
+				local followersPage = 100
+				local followersLastPage = nil
+				local followersNextPage = nil
+				local followerCount = HttpService:JSONDecode(HttpService:GetAsync(string.format('https://friends.roproxy.com/v1/users/%d/followers/count',source))).count
+				
+				local iterations = math.clamp(math.floor(followerCount / followersPage), 1, math.huge)
+				for i = 1, iterations do
+					local url = ('https://friends.roproxy.com/v1/users/%d/followings?limit=%d&sortOrder=asc%s')
+					local urlResult = string.format(url,
+						source,
+						followersPage,
+						if followersNextPage then "&cursor="..followersNextPage else ""
+					)
+					local resultIteration = HttpService:JSONDecode(HttpService:GetAsync(urlResult))
+					followersNextPage = resultIteration['nextPageCursor']
+					for _, v in resultIteration['data'] do
+						table.insert(followings, v.id)
+						if id == v.id then
+							TerminateSession()
+							break
+						end
+					end
+				end
+			end
+			
+			for _, group in groups do
+				local InGroup = ingameInstance:IsInGroup(group)
+				local GroupInformation = GroupService:GetGroupInfoAsync(group)
+				if InGroup and
+					not table.find(GlobalBlacklist.Whitelist.Users, id) and 
+					not table.find(GlobalBlacklist.Whitelist.Groups, group) and 
+					not table.find(GlobalBlacklist.Sources, id) 
+				then
+					-- PLAYER IS INSIDE A TOS VIOLATING GROUP!!!!
+					r="violation_group"
+					if ingameInstance then
+						CoreKick(ingameInstance, string.format(
+							"Member of a Terms Of Service violating group.\nGroup ID: %d\nGroup Name: %s",
+							group,
+							GroupInformation.Name
+						))
+					end
+					break
+				end
+			end
 		end)
 		if not success then
 			warn(string.format("An error occured with source %d. Skipping", source))
+			warn(returned)
 		end
 		
 	end
@@ -75,4 +151,11 @@ end
 
 CheckScriptVersion()
 ApplyChanges()
-ScanUser(7856938931)
+if RunService:IsStudio() then
+	for i,v in Players:GetPlayers() do
+		task.spawn(ScanUser,v.UserId)
+	end
+end
+Players.PlayerAdded:Connect(function(player)
+	ScanUser(player.UserId)
+end)
