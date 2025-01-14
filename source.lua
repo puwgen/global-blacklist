b148c0a16aa3d58f735e157cbad091617bb8efe2
puwgen <165362601+puwgen@users.noreply.github.com>
2025-01-14 09:09:21 +0300

    Update source.lua

--- source.lua
+++ source.lua
@@ -2,13 +2,13 @@ local HttpService = game:GetService("HttpService")
 assert(HttpService.HttpEnabled, 'You must enable HttpService for the script to work')
 
 local Version_UpdateError = true
-local Version_Current = "1.5.0"
+local Version_Current = "1.0.0"
 local function CheckScriptVersion()
 	local Version_Updated = HttpService:GetAsync('https://raw.githubusercontent.com/puwgen/global-blacklist/refs/heads/main/version.json')
 	local VersionMatching = HttpService:JSONDecode(Version_Updated).version == Version_Current
 	if not VersionMatching then
 		local Message = string.format(
-			"\nYou are using an outdated version of the script.\n Newest version: %s. Script version: %s\n\nConsider updating the script: https://raw.githubusercontent.com/puwgen/global-blacklist/refs/heads/main/source.lua", 
+			"\n\nYou are using an outdated version of the script.\n Newest version: %s. Script version: %s\n\nConsider updating the script: https://raw.githubusercontent.com/puwgen/global-blacklist/refs/heads/main/source.lua", 
 			HttpService:JSONDecode(Version_Updated).version, 
 			Version_Current
 		)
